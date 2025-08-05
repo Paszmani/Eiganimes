e84cee256bf3a79134897a23e9aacb2768017af2
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-05 16:30:59 -0300

    Created user entity to user register

---
+++ eiganimes/src/main/java/com/eiganimes/entity/User.java
@@ -0,0 +1,30 @@
+package com.eiganimes.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+
+}
