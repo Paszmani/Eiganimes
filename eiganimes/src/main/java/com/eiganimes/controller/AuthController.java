8a18bdcd9f99e3e0e4576e32d74c2e9a8d588638
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-05 16:55:53 -0300

    Update AuthController

--- eiganimes/src/main/java/com/eiganimes/controller/AuthController.java
+++ eiganimes/src/main/java/com/eiganimes/controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/eiganimes/auth")
 public class AuthController {
 
     private final UserService userService;
