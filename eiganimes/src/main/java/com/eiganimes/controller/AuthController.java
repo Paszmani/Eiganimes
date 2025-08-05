aaf6870d1e2fb7a78cae291a4de1050e75bf0a4e
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-05 16:30:44 -0300

    Created auth controller to user register

---
+++ eiganimes/src/main/java/com/eiganimes/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.eiganimes.controller;
+
+import com.eiganimes.controller.request.UserRequest;
+import com.eiganimes.controller.response.UserResponse;
+import com.eiganimes.entity.User;
+import com.eiganimes.mapper.UserMapper;
+import com.eiganimes.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody UserRequest request) {
+        User savedUser = userService.save(UserMapper.toUser(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserResponse((savedUser)));
+    }
+
+}
