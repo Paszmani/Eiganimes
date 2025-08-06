87d3b789b7d5b834df467ec1e9108df8d392de3b
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-05 23:24:50 -0300

    Updated AuthController

--- eiganimes/src/main/java/com/eiganimes/controller/AuthController.java
+++ eiganimes/src/main/java/com/eiganimes/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.eiganimes.controller;
 
+import com.eiganimes.config.TokenService;
+import com.eiganimes.controller.request.LoginRequest;
 import com.eiganimes.controller.request.UserRequest;
+import com.eiganimes.controller.response.LoginResponse;
 import com.eiganimes.controller.response.UserResponse;
 import com.eiganimes.entity.User;
 import com.eiganimes.mapper.UserMapper;
@@ -8,6 +11,9 @@ import com.eiganimes.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +25,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@RequestBody UserRequest request) {
         User savedUser = userService.save(UserMapper.toUser(request));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserResponse((savedUser)));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        Authentication authenticate = authenticationManager.authenticate(userAndPass);
+
+        User user = (User) authenticate.getPrincipal();
+
+        String token = tokenService.generateToken(user);
+
+        return ResponseEntity.ok(new LoginResponse(token));
+
+
     }
 
 }
