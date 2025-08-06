b95948c0a3fab51bd9ed084b3a07bb3c30f4cb14
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-06 00:46:06 -0300

    Updated StreamingRequest

--- eiganimes/src/main/java/com/eiganimes/controller/request/StreamingRequest.java
+++ eiganimes/src/main/java/com/eiganimes/controller/request/StreamingRequest.java
@@ -1,7 +1,8 @@
 package com.eiganimes.controller.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
 @Builder
-public record StreamingRequest(String name) {
+public record StreamingRequest(@NotEmpty(message = "Streaming name is required")String name) {
 }
