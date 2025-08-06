27112876509fe83e7d84f829ca22735394573338
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-05 23:37:39 -0300

    Created ApplicationControllerAdvice

---
+++ eiganimes/src/main/java/com/eiganimes/config/ApplicationControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.eiganimes.config;
+
+import com.eiganimes.exception.UsernameOrPasswordInvalidException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(UsernameOrPasswordInvalidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleNotFoundException(UsernameOrPasswordInvalidException ex) {
+        return ex.getMessage();
+    }
+
+}
