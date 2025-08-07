b3c3516fa1f91f0a6618fe9a87e6c5875ae3df2e
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-07 13:12:43 -0300

    Rote configuration in spring security.

--- eiganimes/src/main/java/com/eiganimes/config/SecurityConfig.java
+++ eiganimes/src/main/java/com/eiganimes/config/SecurityConfig.java
@@ -31,6 +31,8 @@ public class SecurityConfig {
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers(HttpMethod.POST, "/eiganimes/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/eiganimes/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/swagger/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
