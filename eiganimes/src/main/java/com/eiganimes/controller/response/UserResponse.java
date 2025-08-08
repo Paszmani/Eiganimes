af0249d8d54855c6f5bca4007cbfb0cd4227b881
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-08 00:07:18 -0300

    Updated UserResponse.

--- eiganimes/src/main/java/com/eiganimes/controller/response/UserResponse.java
+++ eiganimes/src/main/java/com/eiganimes/controller/response/UserResponse.java
@@ -4,10 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record UserResponse(@Schema(type = "long", description = "User response id")
-                           Long id,
-                           @Schema(type = "string", description = "User response name")
+public record UserResponse(Long id,
                            String name,
-                           @Schema(type = "string", description = "User response email")
                            String email) {
 }
