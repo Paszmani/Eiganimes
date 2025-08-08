5ad896833570987f3b90973ebd3d794f247e2f09
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-07 23:31:45 -0300

    Updated AnimeResponse.

--- eiganimes/src/main/java/com/eiganimes/controller/response/AnimeResponse.java
+++ eiganimes/src/main/java/com/eiganimes/controller/response/AnimeResponse.java
@@ -8,19 +8,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Builder
-public record AnimeResponse(@Schema(type = "long", description = "Anime id")
-                            Long id,
-                            @Schema(type = "string", description = "Anime name")
+public record AnimeResponse(Long id,
                             String title,
-                            @Schema(type = "string", description = "Anime description")
                             String description,
                             @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
-                            @Schema(type = "date", description = "Anime release date")
                             LocalDate releaseDate,
-                            @Schema(type = "double", description = "Anime rating. ex: 8.9")
                             double rating,
-                            @Schema(type = "array", description = "Category codes list")
                             List<CategoryResponse> categories,
-                            @Schema(type = "string", description = "Streaming service codes list")
                             List<StreamingResponse> streamings) {
 }
