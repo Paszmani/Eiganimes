96d4209ad9dfbf8ab31cf7582eff833ea63b6512
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-07 23:41:51 -0300

    Updated LoginResponse.

--- eiganimes/src/main/java/com/eiganimes/controller/response/LoginResponse.java
+++ eiganimes/src/main/java/com/eiganimes/controller/response/LoginResponse.java
@@ -2,6 +2,5 @@ package com.eiganimes.controller.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record LoginResponse(@Schema(type = "string", description = "Login response token")
-                            String token) {
+public record LoginResponse(String token) {
 }
