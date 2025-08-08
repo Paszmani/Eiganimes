8292fb87681032f59ce68fb195dabe935a1fbcba
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-07 23:54:45 -0300

    Updated UserRequest.

--- eiganimes/src/main/java/com/eiganimes/controller/request/UserRequest.java
+++ eiganimes/src/main/java/com/eiganimes/controller/request/UserRequest.java
@@ -2,10 +2,7 @@ package com.eiganimes.controller.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record UserRequest(@Schema(type = "string", description = "User name request")
-                          String name,
-                          @Schema(type = "string", description = "User email request")
+public record UserRequest(String name,
                           String email,
-                          @Schema(type = "string", description = "User password request")
                           String password) {
 }
