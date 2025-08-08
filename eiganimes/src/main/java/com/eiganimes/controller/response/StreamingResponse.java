0bcf01d87c092dde7cad9b897bc74c7e2377d61c
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-07 23:50:29 -0300

    Updated StreamingResponse.

--- eiganimes/src/main/java/com/eiganimes/controller/response/StreamingResponse.java
+++ eiganimes/src/main/java/com/eiganimes/controller/response/StreamingResponse.java
@@ -4,8 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record StreamingResponse(@Schema(type = "long", description = "Streaming id response")
-                                Long id,
-                                @Schema(type = "string", description = "Streaming name response")
+public record StreamingResponse(Long id,
                                 String name) {
 }
