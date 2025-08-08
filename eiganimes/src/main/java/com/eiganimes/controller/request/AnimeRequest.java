d332245a69dd9d1fed5757202799a26a413c3615
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-07 23:31:38 -0300

    Updated AnimeRequest.

--- eiganimes/src/main/java/com/eiganimes/controller/request/AnimeRequest.java
+++ eiganimes/src/main/java/com/eiganimes/controller/request/AnimeRequest.java
@@ -7,18 +7,12 @@ import jakarta.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 import java.util.List;
 
-public record AnimeRequest(@Schema(type = "string", description = "Anime name")
-                           @NotEmpty(message = "Anime title is required")
+public record AnimeRequest(@NotEmpty(message = "Anime title is required")
                            String title,
-                           @Schema(type = "string", description = "Anime description")
                            String description,
                            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
-                           @Schema(type = "date", description = "Anime release date. ex: '24/09/2003'")
                            LocalDate releaseDate,
-                           @Schema(type = "double", description = "Anime rating. ex: 8.9")
                            double rating,
-                           @Schema(type = "array", description = "Category codes list")
                            List<Long> categories,
-                           @Schema(type = "array", description = "Streaming service codes list")
                            List<Long> streamings) {
 }
