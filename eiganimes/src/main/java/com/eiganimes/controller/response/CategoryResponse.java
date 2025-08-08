bc1d5dbe64bd1c1c59df5b88392e8cdf3bcf824b
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-07 23:32:15 -0300

    Updated CategoryResponse.

--- eiganimes/src/main/java/com/eiganimes/controller/response/CategoryResponse.java
+++ eiganimes/src/main/java/com/eiganimes/controller/response/CategoryResponse.java
@@ -4,9 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record CategoryResponse(@Schema(type = "long", description = "Category id response")
-                               Long id,
-                               @Schema(type = "string", description = "Category name response")
+public record CategoryResponse(Long id,
                                String name) {
 
 }
