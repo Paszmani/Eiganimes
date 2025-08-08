71160511612b69ae096f6d2f81cf3860ddb598e2
PassamaniArthur <passamaniarthur@gmail.com>
2025-08-07 23:30:38 -0300

    Updated AnimeController.

--- eiganimes/src/main/java/com/eiganimes/controller/AnimeController.java
+++ eiganimes/src/main/java/com/eiganimes/controller/AnimeController.java
@@ -2,57 +2,68 @@ package com.eiganimes.controller;
 
 import com.eiganimes.controller.request.AnimeRequest;
 import com.eiganimes.controller.response.AnimeResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import com.eiganimes.entity.Anime;
+import com.eiganimes.mapper.AnimeMapper;
+import com.eiganimes.service.AnimeService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
-@Tag(name = "Anime", description = "Resource responsible for anime management.")
-public interface AnimeController {
 
-    @Operation(summary = "Save anime", description = "Method responsible to save a new Anime.",
-            security = @SecurityRequirement(name = "bearerAuth"))
-    @ApiResponse(responseCode = "201", description = "Anime saved with success",
-            content = @Content(schema = @Schema(implementation = AnimeResponse.class)))
-    ResponseEntity<AnimeResponse> save(@Valid @RequestBody AnimeRequest request);
+@RestController()
+@RequestMapping("/eiganimes/anime")
+@RequiredArgsConstructor
+public class AnimeController {
 
-    @Operation(summary = "Search animes", description = "Method responsible to return all registered animes.",
-            security = @SecurityRequirement(name = "bearerAuth"))
-    @ApiResponse(responseCode = "200", description = "Return all registered animes",
-            content = @Content(array = @ArraySchema(schema = @Schema(implementation = AnimeResponse.class))))
-    ResponseEntity<List<AnimeResponse>> findAll();
+    private final AnimeService animeService;
 
-    @Operation(summary = "Search anime by id", description = "Method responsible to search animes by id.",
-            security = @SecurityRequirement(name = "bearerAuth"))
-    @ApiResponse(responseCode = "200", description = "Anime found with success",
-            content = @Content(schema = @Schema(implementation = AnimeResponse.class)))
-    @ApiResponse(responseCode = "404", description = "Anime not found", content = @Content())
-    ResponseEntity<AnimeResponse> findById(@PathVariable Long id);
+    @PostMapping
+    public ResponseEntity<AnimeResponse> save(@Valid @RequestBody AnimeRequest request) {
+        Anime savedAnime = animeService.save(AnimeMapper.toAnime(request));
+        return ResponseEntity.ok(AnimeMapper.toAnimeResponse(savedAnime));
+    }
 
-    @Operation(summary = "Update animes", description = "Method responsible to update animes.",
-            security = @SecurityRequirement(name = "bearerAuth"))
-    @ApiResponse(responseCode = "200", description = "Anime updated with success",
-            content = @Content(schema = @Schema(implementation = AnimeResponse.class)))
-    @ApiResponse(responseCode = "404", description = "Anime not found", content = @Content())
-    ResponseEntity<AnimeResponse> update(@Valid @PathVariable Long id, @RequestBody AnimeRequest request);
+    @GetMapping
+    public ResponseEntity<List<AnimeResponse>> findAll() {
+        return ResponseEntity.ok(animeService.findAll()
+                .stream()
+                .map(AnimeMapper::toAnimeResponse)
+                .toList());
+    }
 
-    @Operation(summary = "Search animes by category", description = "Method responsible to return animes by category",
-            security = @SecurityRequirement(name = "bearerAuth"))
-    @ApiResponse(responseCode = "200", description = "Return animes by category",
-            content = @Content(array = @ArraySchema(schema = @Schema(implementation = AnimeResponse.class))))
-    ResponseEntity<List<AnimeResponse>> findByCategory(@RequestParam Long category);
+    @GetMapping("/{id}")
+    public ResponseEntity<AnimeResponse> findById(@PathVariable Long id) {
+        return animeService.findById(id)
+                .map(anime -> ResponseEntity.ok(AnimeMapper.toAnimeResponse(anime)))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
-    @Operation(summary = "Delete animes", description = "Method responsible to delete animes.",
-            security = @SecurityRequirement(name = "bearerAuth"))
-    @ApiResponse(responseCode = "204", description = "Anime deleted with success", content = @Content())
-    @ApiResponse(responseCode = "404", description = "Anime not found", content = @Content())
-    ResponseEntity<Void> delete(@PathVariable Long id);
+    @PutMapping("/{id}")
+    public ResponseEntity<AnimeResponse> update(@Valid @PathVariable Long id, @RequestBody AnimeRequest request) {
+        return animeService.update(id, AnimeMapper.toAnime(request))
+                .map(anime -> ResponseEntity.ok(AnimeMapper.toAnimeResponse(anime)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<AnimeResponse>> findByCategory(@RequestParam Long category) {
+        return ResponseEntity.ok(animeService.findByCategory(category)
+                .stream()
+                .map(AnimeMapper::toAnimeResponse)
+                .toList());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Optional<Anime> optionalAnime = animeService.findById(id);
+        if(optionalAnime.isPresent()) {
+            animeService.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
